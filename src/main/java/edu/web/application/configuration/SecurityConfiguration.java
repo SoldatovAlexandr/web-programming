2a1fbae0816c28787a36059bec4bd05463cc4d3c
akorzh <alexey.korzh.kas@gmail.com>
2021-10-01 15:56:56 +0600

    WEBPRO-12:Исправление ошибки расшифровки JWT

--- src/main/java/edu/web/application/configuration/SecurityConfiguration.java
+++ src/main/java/edu/web/application/configuration/SecurityConfiguration.java
@@ -63,7 +63,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(frontendUrl, "http://localhost:8081/"));
+        configuration.setAllowedOrigins(List.of(frontendUrl, "http://localhost:8088/"));
         configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
         configuration.setAllowCredentials(true);
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
